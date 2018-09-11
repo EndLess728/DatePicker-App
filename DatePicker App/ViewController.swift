4baadfe5a4a4b824d43182dda33988788e22af7f
EndLess728 <aryanmk.mk5@gmail.com>
2018-09-11 12:02:14 +0530

    DatePicker With Customized Format

--- DatePicker App/ViewController.swift
+++ DatePicker App/ViewController.swift
@@ -10,16 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myDatePicker: UIDatePicker!
+    @IBOutlet weak var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
 
+    @IBAction func sendDate(_ sender: UIButton) {
+        
+        let myFormatter = DateFormatter()
+        myFormatter.dateFormat = "yyyy-MM-dd"
+        
+        let selectedDate = myFormatter.string(from: myDatePicker.date)
+    
+        myLabel.text =  "\(selectedDate)"
+    }
+    
 }
 
